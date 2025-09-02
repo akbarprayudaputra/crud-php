84d8bad3076ad06da5c7d70c7003d8676fc4fc72
Akbar Prayuda <akbarprayuda210806@gmail.com>
2025-09-02 13:03:33 +0700

    Initial commit and Route Setup

---
+++ App/Config/Router.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Config;
+
+class Router
+{
+  private $routes = [];
+
+  public function get($uri, $action)
+  {
+    $this->addRoute('GET', $uri, $action);
+  }
+
+  public function post($uri, $action)
+  {
+    $this->addRoute('POST', $uri, $action);
+  }
+
+  public function put($uri, $action)
+  {
+    $this->addRoute('PUT', $uri, $action);
+  }
+
+  public function delete($uri, $action)
+  {
+    $this->addRoute('DELETE', $uri, $action);
+  }
+
+  private function addRoute($method, $uri, $action)
+  {
+    $this->routes[$method][$uri] = $action;
+  }
+
+  public function dispatch($method, $uri)
+  {
+    $uri = rtrim($uri, '/') ?: '/';
+
+    $action = $this->routes[$method][$uri] ?? null;
+
+    if (!$action) {
+      http_response_code(404);
+      echo "404 Not Found";
+      return;
+    }
+
+    [$controller, $method] = $action;
+    (new $controller)->$method();
+  }
+}
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Config\Router;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+
+require_once __DIR__ . '/../Routes/web.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$router->dispatch($method, $uri);
